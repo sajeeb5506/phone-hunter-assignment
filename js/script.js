08f7f0287028a8942d3174f0a7cd29b8cbf173fc
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-02-28 22:49:31 +0600

    display phone

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const allPhones =()=>{
+    const inputValue = document.getElementById('input-box').value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
+
+fetch(url)
+.then(res=>res.json())
+.then(data=>showPhoneData(data.data))
+    
+    // console.log(url);
+
+}
+const showPhoneData=(phones)=>{
+
+    for(const phone of phones){
+
+        const parentDiv = document.getElementById('phone-container');
+        const div = document.createElement('div');
+        div.innerHTML=`
+        
+        <div  class="col">
+         
+        <div class="card rounded-3">
+          <img  src="${phone.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Phone Model:${phone.phone_name} </h5>
+            <h5 class="card-title">Phone Brand: ${phone.brand}</h5>
+            <p class="card-text"> Discription: ${phone.slug}</p>
+            <button class="btn btn-primary">Diteals</button>
+          </div>
+        </div>
+
+      </div>
+                             
+        `
+        parentDiv.appendChild(div);
+
+        // console.log(phone);
+    }
+    
+
+}
